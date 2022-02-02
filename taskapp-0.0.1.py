be514db1a06a3fec54f8a48eb5c8d48a6d118606
planiX <p1aniX@protonmail.com>
2022-02-02 14:27:24 +0500

    Script development

--- taskapp-0.0.1.py
+++ taskapp-0.0.1.py
@@ -4,7 +4,7 @@ import sys
 import json
 
 HomeDir = os.environ["HOME"]
-dataLoc = HomeDir + "/NextCloud/Todo"
+dataLoc = HomeDir
 
 # load todos
 def readTodo():
@@ -20,21 +20,39 @@ def readTodo():
 def printList():
     pass
 
+# help command
+def help(args=[]):
+    if args:
+        pass
+    else:
+        print('none')
+
 # add command
-def add():
+def add(args=[]):
     task = input('Title: ')
+    due = input('Due: ')
+    diff = input('Difficulty: ')
 
 
 
 # commands
-commands = {'a': add}
+commands = {
+    'a': add,
+    'h': help
+            }
 
 run = True
 #  main loop
 while run is True:
-    os.system('\clear')
+#    os.system('\clear')
     todoData = readTodo()
     printList()
 
+
     command = input("Command~> ")
-    commands[command]
+    command.strip()
+    if ' ' in command:
+        command = command.split(' ')
+        commands[command[0]](command[1:])
+    else:
+        commands[command]()
